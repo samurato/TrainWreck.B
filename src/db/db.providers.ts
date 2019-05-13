6fd6e3938d4f2f519f05736fd6cd207ad55207d9
Ben Franzi <benfranzi@outlook.com>
2019-05-13 17:45:57 +1000

    fixed start scripts

--- src/db/db.providers.ts
+++ src/db/db.providers.ts
@@ -6,10 +6,13 @@ export const dbProviders =
         provide: 'DbConnectionToken',
         useFactory: async () => await createConnection({
             type: 'postgres',
-            url: config.db.DATABASE_URL,
+            url: 'postgres://bhgykoevqflkwf:5a0eaa362d46ec2de81ae3521fbfecf315d4520bc0993297c99d3e4d04a9ccad@ec2-54-225-116-36.compute-1.amazonaws.com:5432/d1mgec9drukloh',//config.db.DATABASE_URL,
             entities: [
                 User,
             ],
+            extra: {
+              ssl: true,
+            },
             synchronize: (process.env.NODE_ENV === 'development'),
         }),
     };
